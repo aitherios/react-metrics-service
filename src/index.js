2ee6fe91ab7a1803160a254256e32411f4247c4e
Renan Mendes Carvalho <aitherios@gmail.com>
2016-06-28 17:49:50 -0300

    Add middlewares to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { withMetricsServiceContext, MetricsServiceContext } from './helpers/metr
 import { withMetricsServiceClient } from './helpers/with-metrics-service-client'
 import { metricsServiceClick } from './helpers/metrics-service-click'
 import debugLog from './middlewares/debug-log'
+import googleAnalytics from './middlewares/google-analytics'
+import googleAnalyticsLegacy from './middlewares/google-analytics-legacy'
+import googleTagManager from './middlewares/google-tag-manager'
+import tealium from './middlewares/tealium'
 
 export {
   createClient,
@@ -11,4 +15,8 @@ export {
   withMetricsServiceClient,
   metricsServiceClick,
   debugLog,
+  googleAnalytics,
+  googleAnalyticsLegacy,
+  googleTagManager,
+  tealium,
 }
